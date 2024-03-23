d89229bd75a37156c7577ce770a854b2947942b1
shopiacrl <162675064+snitaloi@users.noreply.github.com>
2024-03-23 15:57:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-1a59f699-3c83-4c6c-abb8-8db5a92347a9/)
 
 
 echo "$curl_output" >> Update.tsx
